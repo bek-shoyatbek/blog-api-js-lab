a990dd5f9e90f3facf17e38f543af084aa8443b5
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2024-10-03 15:13:26 +0500

    feat: custom error and errorHandler middleware added

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import "reflect-metadata";
+import { errorHandler } from "./common/middlewares/error-handler.middleware";
 
 export const app = express();
 
 app.use(express.json());
+
+app.use("*", errorHandler);
