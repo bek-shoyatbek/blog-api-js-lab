c4891b8b28db34987a2a78629e1d4a435a74c008
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2024-10-07 16:48:35 +0500

    feat: test datasource reverted to postgres

--- src/common/configs/index.ts
+++ src/common/configs/index.ts
@@ -21,11 +21,17 @@ export const dbConfig: DataSourceOptions = {
 };
 
 export const testDbConfig: DataSourceOptions = {
-  type: "sqlite",
-  database: `${process.cwd()}/database.sql`,
+  type: "postgres",
+  host: process.env.DB_HOST,
+  port: +(process.env.DB_PORT || 5432),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_TEST_DATABASE,
   entities: [User, Blog, Comment],
   synchronize: true,
   logging: true,
+  migrations: [],
+  entitySkipConstructor: true,
 };
 
 export const appConfig = {
