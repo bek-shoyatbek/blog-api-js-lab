c558eae9d23d07d6f06e822be810ca1e12e09641
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2024-10-08 22:30:40 +0500

    feat: blog (crud) completed with tests

--- src/modules/blogs/blog.controller.test.ts
+++ src/modules/blogs/blog.controller.test.ts
@@ -1,14 +1,14 @@
 import request from "supertest";
 import { app } from "../../app";
 import { beforeAll, describe, expect, it } from "@jest/globals";
-import { initializeDataSource } from "../../common/database";
+import { initializeTestDataSource } from "../../common/database";
 
 // ! Please replace token if expires
 const authToken =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIxOWE5NjlhZi1jYjZmLTRjOTUtYWU3OC0wNDAwZWMwZDU0NzgiLCJlbWFpbCI6ImJla0BnbWFpbC5jb20iLCJpYXQiOjE3MjgyODAzMjAsImV4cCI6MTcyODI4NzUyMH0.0MEQb_KbFmYL43pYGIINzQXml7jzhmGfJbaborKEOjI";
 
 beforeAll(async () => {
-  await initializeDataSource();
+  await initializeTestDataSource();
 });
 
 describe("Blog controller tests: ", () => {
@@ -104,5 +104,24 @@ describe("Blog controller tests: ", () => {
     mockBlogObjProps.forEach((prop) => {
       expect(data).toHaveProperty(prop);
     });
+  });
+
+  it("should fetch blogs with pagination", async () => {
+    const take = 2;
+    const skip = 1;
+    const response = await request(app)
+      .get(`/blogs?take=${take}&skip=${skip}`)
+      .set("Authorization", "Bearer " + authToken)
+      .set("Content-Type", "application/json");
+
+    expect(response.status).toBe(200);
+
+    const body = response.body;
+    expect(body).toHaveProperty("data");
+    expect(body).toHaveProperty("message");
+
+    const blogs = body?.data;
+    expect(blogs).toBeInstanceOf(Array);
+    expect(blogs.length).toBe(2);
   });
 });
