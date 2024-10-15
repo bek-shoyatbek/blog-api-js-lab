7f6a01a08d8afb4ce303648b8202c73348e74967
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2024-10-15 11:45:08 +0500

    feat: auth router registered

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import "reflect-metadata";
+import { AuthRouter } from "./modules/auth/auth.router";
 
 export const app = express();
 
 app.use(express.json());
+
+app.use("/auth", AuthRouter);
