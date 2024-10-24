820b7d723a40bd52f6b5ea356d5f75bcd4b3813c
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2024-10-24 19:50:03 +0500

    feat: commentRepository exported

--- src/common/database/index.ts
+++ src/common/database/index.ts
@@ -1,7 +1,9 @@
 import { AppDataSource } from "./datasource";
 import { Blog } from "./entities/blog.entity";
+import { Comment } from "./entities/comment.entity";
 import { User } from "./entities/user.entity";
 
 export * from "./datasource";
 export const userRepository = AppDataSource.getRepository(User);
 export const blogRepository = AppDataSource.getRepository(Blog);
+export const commentRepository = AppDataSource.getRepository(Comment);
