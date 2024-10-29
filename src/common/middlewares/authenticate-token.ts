7606650dd95c1e0156705f78f1d35fd51e214768
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2024-10-29 20:45:21 +0500

    feat: authenticate-token updated according to rbac

--- src/common/middlewares/authenticate-token.ts
+++ src/common/middlewares/authenticate-token.ts
@@ -1,7 +1,7 @@
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Response } from "express";
 import jwt from "jsonwebtoken";
 import { appConfig } from "../configs";
-import { AuthRequest } from "../../types";
+import { AuthRequest, UserPayload } from "../../types";
 
 export async function authenticateToken(
   req: AuthRequest,
@@ -11,16 +11,16 @@ export async function authenticateToken(
   try {
     const authHeader = req.headers["authorization"];
     const token = authHeader && authHeader.split(" ")[1];
+
     if (!token) {
-      res.status(401).json({ message: "Token not provided!" });
-      return;
+      return res.status(401).json({ message: "Token not provided!" });
     }
+
     jwt.verify(token, appConfig.jwtSecret as string, (err, user) => {
       if (err) {
-        res.status(403).json({ message: "Couldn't verify token" });
-        return;
+        return res.status(403).json({ message: "Couldn't verify token" });
       }
-      req.user = user;
+      req.user = user as UserPayload;
       next();
     });
   } catch (err) {
